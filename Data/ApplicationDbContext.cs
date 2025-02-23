28b2bebd39ab8d144bc0a1a156751e45fd7db3e8
Mia Blažeković <mia.blazekovic0@gmail.com>
2025-02-23 20:21:00 +0100

    Seeding proces za tablice: USERS i ROLES (javlja se greška pa je dodatno dodana konfiguracija da se ignorira)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.Xml;
 
 namespace LeaveManagementSystem.Web.Data
 {
@@ -16,6 +19,76 @@ namespace LeaveManagementSystem.Web.Data
         /// </summary>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+        }
+
+        /// <summary>
+        /// Ne znam zašto mi ne prolazi database-update za migraciju ispod (seeding roles and users)
+        /// Tekst greške koji se javlja: The model for context 'ApplicationDbContext' changes each time it is built. This is usually caused by dynamic values used in a 'HasData' call
+        /// Uglavnom, konfigurirala sam da sustav jednostavno ignorira tu grešku
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
+
+        /// <summary>
+        /// Data-seeding proces postavljanja custom-uloga za korisnike (data-model: IdentityRole)
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Base-call ne smije obrisati:
+            base.OnModelCreating(builder);
+
+            //Kôd koji omogućuje data-seeding proces za dodavanje inicijalnih uloga:
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "6b64a0a3-b68f-4222-9244-4e70dd5a681d", //DbContext dozvoljava INSERT id-eva zato što se radi o data-seeding procesu
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE"
+                },
+                new IdentityRole
+                {
+                    Id = "42a465c4-e747-4314-8c06-e7f0d0efff98", //korišten je GUID generator
+                    Name = "Supervisor",
+                    NormalizedName = "SUPERVISOR"
+                },
+                new IdentityRole
+                {
+                    Id = "939a4b2f-fa07-46c3-b7d1-538e247a5055", //korišten je GUID generator
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                }
+             );
+
+            //NE RADI korištenje hashera zato što javlja grešku:
+            //"The model for context 'ApplicationDbContext' changes each time it is built. This is usually caused by dynamic values used in a 'HasData' call"
+            //var hasher = new PasswordHasher<IdentityUser>();
+            //var lozinka = hasher.HashPassword(null, "Password1@");
+
+            //Data-seeding proces sa postavljanje inicijalnog korisnika (insanca klase IdentityUser):
+            builder.Entity<IdentityUser>().HasData(
+                new IdentityUser
+                {
+                    Id = "a0c89e17-6dae-4d39-b35c-0d54a8dad74e",
+                    Email = "admin@localhost.com",
+                    NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                    UserName = "admin@localhost.com",
+                    PasswordHash = "AQAAAAIAAYagAAAAEEr1h1mY9FjUylM1az+zXrrcHcPZ5g/NC/6j1smENaJgPqIjH1KPjoU0MepWy9lHxA==", // hardkodirani hash
+                    EmailConfirmed = true
+                }
+            );
+
+            //Konfiguracija: povezivanje defaultnog ADMIN usera sa ADMINISTRATOR ulogom
+            builder.Entity<IdentityUserRole<string>>().HasData
+            (
+                new IdentityUserRole<string>
+                {
+                    RoleId = "939a4b2f-fa07-46c3-b7d1-538e247a5055", //id administrating uloge
+                    UserId = "a0c89e17-6dae-4d39-b35c-0d54a8dad74e" //id admin korisnika
+                }
+            );
         }
 
         /// <summary>
---
+++ Data/Migrations/20250223191816_SeedingUserRolesAndIdentity.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LeaveManagementSystem.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingUserRolesAndIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "42a465c4-e747-4314-8c06-e7f0d0efff98", null, "Supervisor", "SUPERVISOR" },
+                    { "6b64a0a3-b68f-4222-9244-4e70dd5a681d", null, "Employee", "EMPLOYEE" },
+                    { "939a4b2f-fa07-46c3-b7d1-538e247a5055", null, "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a0c89e17-6dae-4d39-b35c-0d54a8dad74e", 0, "32b50dd4-4b75-485f-8a27-0ce2ff63b200", "admin@localhost.com", true, false, null, "ADMIN@LOCALHOST.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEEr1h1mY9FjUylM1az+zXrrcHcPZ5g/NC/6j1smENaJgPqIjH1KPjoU0MepWy9lHxA==", null, false, "4b3d4356-23c4-4a6d-9c7c-732ba9bbbb78", false, "admin@localhost.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "939a4b2f-fa07-46c3-b7d1-538e247a5055", "a0c89e17-6dae-4d39-b35c-0d54a8dad74e" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "42a465c4-e747-4314-8c06-e7f0d0efff98");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6b64a0a3-b68f-4222-9244-4e70dd5a681d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "939a4b2f-fa07-46c3-b7d1-538e247a5055", "a0c89e17-6dae-4d39-b35c-0d54a8dad74e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "939a4b2f-fa07-46c3-b7d1-538e247a5055");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a0c89e17-6dae-4d39-b35c-0d54a8dad74e");
+        }
+    }
+}
